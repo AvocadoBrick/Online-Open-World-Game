47991ebea247c0f76ad2cc761e7cf048d64fd105
Avocado Brick <77745393+AvocadoBrick@users.noreply.github.com>
2021-01-20 19:24:10 +0200

    Add files via upload

---
+++ Instatiate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Instatiate : MonoBehaviour
+{
+    [SerializeField] private GameObject PLayerPrefab = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Instantiate(PLayerPrefab.name, new Vector3(Random.Range(-10.0f, 10.0f), 2f, 0), Quaternion.identity);
+    }
+}
