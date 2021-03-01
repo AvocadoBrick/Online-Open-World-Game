b681d5a5bfe0999bc6e7cde7cd569df90773a59f
Avocado Brick <77745393+AvocadoBrick@users.noreply.github.com>
2021-03-01 21:47:16 +0200

    Add files via upload

---
+++ RPCscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class RPCscript : MonoBehaviourPun
+{
+    private Text text;
+    public InputField inpt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject canvasobject = GameObject.FindGameObjectWithTag("text");
+        text = canvasobject.GetComponentInChildren<Text>();
+    }
+    [PunRPC]
+    void changetext(string textRPC)
+    {
+        text.text = textRPC;
+    }
+    public void callrpc()
+    {
+        photonView.RPC("changetext", RpcTarget.All, inpt.text);
+    }
+}
