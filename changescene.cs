591cdac5afcf7674223bdc265e13512f7168f76b
Avocado Brick <77745393+AvocadoBrick@users.noreply.github.com>
2021-04-11 20:03:45 +0300

    Add files via upload

---
+++ changescene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviourPun
+{
+    public GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public void sceneselect()
+    {
+        SceneManager.LoadScene("Game 1");
+    }
+}
